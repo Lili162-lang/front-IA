f655c06d0f83163ab1ebd6c4d23b53e360f00e08
Lili1823 <lilianarivera310@gmail.com>
2025-09-01 22:06:05 -0500

    arquitectura check

---
+++ src/app/admin/components/admin-category-tree/admin-category-tree.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-category-tree',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-category-tree.component.html',
+  styleUrl: './admin-category-tree.component.css'
+})
+export class AdminCategoryTreeComponent {
+
+}
